456e41641291cf8034bbe62229ad76b2b9219f78
Marat Rafigullin <email.marat@gmail.com>
2022-10-21 19:00:45 +0300

    added some functions

--- controller.py
+++ controller.py
@@ -8,14 +8,25 @@ def run():
         res = model.csv_data_open()
         view.show_res(res)
     elif start == 2:
-        in_info = view.add_info()
-        model.add_info(in_info)
+        # in_info = view.add_info()
+        # model.add_info(in_info)
+        add_again = 0   # Добавление повторного запроса на ввод информации
+        while add_again != 1:
+            in_info = view.add_info()
+            model.add_info(in_info)
+            add_again = view.add_info_again()
     elif start == 3:
+        view.show_res(model.csv_data_open()) # Добавлен вывод списка
         index = view.delete()
         model.del_info(index)
     elif start == 4:
+        view.show_res(model.csv_data_open()) # Добавлен вывод списка
         index, tel = view.change_tel()
         model.update_info(index, tel)
+    elif start == 5:     # Добавлена возможность экспорта записей в txt формат
+        model.import_into_csv()
+        view.show_export_txt()
+        
 
 
 # run()
--- model.py
+++ model.py
@@ -2,39 +2,40 @@ import csv
 
 
 def csv_data_open():      # 1 - показать все
-    with open("010_Seminar10/csv_data.csv", encoding='utf-8') as file:
+    with open("csv_data.csv", encoding='utf-8') as file:
         file_csv = csv.reader(file, delimiter=";")
         res = list(file_csv)
     return res
 
-
 def add_info(list):  # 2- добавление информации
-    with open("010_Seminar10/csv_data.csv", "a", encoding="utf8", newline='') as f:
+    with open("csv_data.csv", "a", encoding="utf8", newline='') as f:
         writer = csv.writer(f, delimiter=';')
         writer.writerow(list)
-
-# list=['Мама', 'папа',  '25544']
-# add_info(list)
-
-
 
 def del_info(index):  # удаление информации
     list_csv = csv_data_open()
     print(list_csv)
     del list_csv[index]
-    with open("010_Seminar10/csv_data.csv", "w", encoding="utf8", newline='') as file:
+    with open("csv_data.csv", "w", encoding="utf8", newline='') as file:
         writer = csv.writer(file, delimiter=';')
         for row in list_csv:
             writer.writerow(row)
 
 
 # add_info(2)
-def update_info(index, tel):  # обнавление информации
+def update_info(index, tel):  # обновление информации
     list_csv = csv_data_open()
     list_csv[index][3] = tel
-    with open("010_Seminar10/csv_data.csv", "w", encoding="utf8", newline='') as file:
+    with open("csv_data.csv", "w", encoding="utf8", newline='') as file:
         writer = csv.writer(file, delimiter=';')
         for row in list_csv:
             writer.writerow(row)
 
-# update_info(0, '+7(495)469476')
+def import_into_csv():   # импорт из csv в txt формат
+    with open("csv_data.csv", encoding='utf-8') as file:
+        file_csv = csv.reader(file)
+        res = list(file_csv)
+    with open("txt_data.txt", 'w') as txt_file:
+        for row in res:
+            txt_file.writelines(row)
+            txt_file.writelines('\n')
